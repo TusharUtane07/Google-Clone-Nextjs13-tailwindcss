3f170102afbdab48571d03b7f525e24e0b2fa6cc
unknown <tusharutane2@gmail.com>
2023-03-23 12:10:49 +0530

    Installation Done

---
+++ src/app/head.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+        <title>Google Clone | Nextjs</title>
+        <meta name="description" content="Google Clone using NEXTJS13" />
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+    </>
+  )
+}
